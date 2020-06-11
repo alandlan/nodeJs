96de33146b44ee7b3c02df77bf52779b41f7bccc
Alan Martins <alanhorcrux@gmail.com>
2020-06-11 17:18:34 -0300

    Criando rota de agendamento

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,10 @@ import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
+
 app.use(routes);
 
 app.listen('3333', () => {
-  console.log('🚀 Server Started!');
+    console.log('🚀 Server Started!');
 });
