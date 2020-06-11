43432a3464a99e4fcf2f1fc84111416d72776c37
Alan Martins <alanhorcrux@gmail.com>
2020-06-11 15:11:46 -0300

    Configurado .editoConfig

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/',(request, response) => {
+    return response.json({message: '🚀 Ok!'});
+})
+
 app.listen('3333',() => {
     console.log('🚀 Server Started!')
 })
